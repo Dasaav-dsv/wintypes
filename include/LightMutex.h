dbd19ddf505b8cf715805c934261978e374ecdc8
Dasaav-dsv <simplydasaav@gmail.com>
2024-02-21 22:01:12 +0100

    Light mutex impl (Critical Section)

--- include/LightMutex.h
+++ include/LightMutex.h
@@ -1,5 +1,28 @@
 #ifndef WINTYPES_INCLUDE_LIGHTMUTEX_H
 #define WINTYPES_INCLUDE_LIGHTMUTEX_H
 
+// Forward declaration to not expose Windows headers
+struct _RTL_CRITICAL_SECTION;
+using CRITICAL_SECTION = _RTL_CRITICAL_SECTION;
+
+namespace WinTypes {
+    // Light mutex that wraps a pointer to a Windows Critical Section
+    // Satisfies the C++ Mutex requirement
+    // Recursive, not TimedLockable
+    class LightMutex {
+        mutable CRITICAL_SECTION* criticalSection;
+    public:
+        LightMutex();
+        ~LightMutex() noexcept;
+
+        void lock() noexcept;
+        void unlock() noexcept;
+        bool try_lock() noexcept;
+
+        // Deleted as per the Mutex requirement
+        LightMutex(const LightMutex&) = delete;
+        LightMutex(LightMutex&&) = delete;
+    };
+}
 
 #endif /* WINTYPES_INCLUDE_LIGHTMUTEX_H */

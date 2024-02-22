f074b9d1fcd3dbb732b0626218c74493643f26f4
Dasaav-dsv <simplydasaav@gmail.com>
2024-02-22 17:24:54 +0100

    32-bit PEB fix

--- source/ibo32.cpp
+++ source/ibo32.cpp
@@ -6,13 +6,17 @@
 
 // First three PEB (Process Environment Block) fields
 struct ProcessEnvironmentBlockExcerpt {
-    char flags[8];
+    char flags[3];
     void* mutant;
     void* imageBaseAddress;
 };
 
 void* getImageBase() noexcept {
     // Access the PEB (Process Environment Block) inside the TEB (Thread Environment Block)
+#if defined(__x86_64__) || defined(_WIN64) // 64-bit
     auto pPEB = reinterpret_cast<ProcessEnvironmentBlockExcerpt*>(__readgsqword(0x60));
+#else // 32-bit
+    auto pPEB = reinterpret_cast<ProcessEnvironmentBlockExcerpt*>(__readfsdword(0x30));
+#endif
     return pPEB->imageBaseAddress;
 }

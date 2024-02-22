f323b2f63327672199298d2739bdedf34ce616f6
Dasaav-dsv <simplydasaav@gmail.com>
2024-02-22 16:36:14 +0100

    Basic ibo32 implementation

---
+++ include/ibo32.h
@@ -0,0 +1,77 @@
+#ifndef WINTYPES_INCLUDE_IBO32_H
+#define WINTYPES_INCLUDE_IBO32_H
+
+#include <cstddef>
+#include <cstdint>
+
+namespace WinTypes {
+    void* getImageBase() noexcept;
+
+    // 31-bit integer offset type used by many Windows internals
+    // Convertible to and from integers and pointers
+    template <typename T>
+    struct ibo32_t {
+        using pointer_t = T*;
+        using const_pointer_t = const T*;
+
+        // Default, integer and pointer constructor
+        ibo32_t() noexcept : value(-1) {}
+        ibo32_t(int value) noexcept : value(value) {}
+        ibo32_t(const_pointer_t address) noexcept
+            : value(this->ptrDiff(getImageBase(), address)) {}
+
+        // Assignment operators (integer and pointer)
+        void operator = (int rhs) noexcept { this->value = rhs; }
+        void operator = (const_pointer_t rhs) noexcept {
+            this->value = this->ptrDiff(getImageBase(), address);
+        }
+
+        // Explicit conversion operators to corresponding pointer types
+        explicit operator pointer_t() noexcept { return this->fromImageBase(getImageBase()); }
+        explicit operator const_pointer_t() const noexcept { return this->fromImageBase(getImageBase()); }
+
+        operator int() const noexcept { return this->value; }
+        explicit operator bool() const noexcept { return this->value >= 0; }
+
+        // Pointer access
+        T& operator *() noexcept { return *static_cast<pointer_t>(*this); }
+        const T& operator *() const noexcept { return *static_cast<const_pointer_t>(*this); }
+		pointer_t operator->() noexcept { return static_cast<pointer_t>(*this); }
+		const_pointer_t operator->() const noexcept { return static_cast<const_pointer_t>(*this); }
+
+        // Comparison operators
+		bool operator == (const ibo32_t<T>& other) const noexcept { return this->value == other.value; }
+		bool operator != (const ibo32_t<T>& other) const noexcept { return this->value != other.value; }
+		bool operator < (const ibo32_t<T>& other) const noexcept { return this->value < other.value; }
+		bool operator > (const ibo32_t<T>& other) const noexcept { return this->value > other.value; }
+		bool operator <= (const ibo32_t<T>& other) const noexcept { return this->value <= other.value; }
+		bool operator >= (const ibo32_t<T>& other) const noexcept { return this->value >= other.value; }
+
+        // Pointer from base + offset
+		pointer_t fromImageBase(void* base) noexcept {
+			if (this->value >= 0) {
+				return reinterpret_cast<pointer_t>((void*)(reinterpret_cast<uintptr_t>(base) + this->value));
+			}
+			else return nullptr;
+		}
+
+        // Pointer from base + offset (const)
+		const_pointer_t fromImageBase(void* base) const noexcept {
+			if (this->value >= 0) {
+				return reinterpret_cast<const_pointer_t>((void*)(reinterpret_cast<uintptr_t>(base) + this->value));
+			}
+			else return nullptr;
+		}
+
+    private:
+        template <typename L, typename R>
+        int ptrDiff(L* l, R* r) {
+            auto diff = reinterpret_cast<uintptr_t>(r) - reinterpret_cast<uintptr_t>(l);
+            return diff >> 31 ? -1 : static_cast<int>(diff);
+        }
+
+        int value;
+    };
+}
+
+#endif /* WINTYPES_INCLUDE_IBO32_H */
---
+++ source/ibo32.cpp
@@ -0,0 +1,18 @@
+#include <ibo32.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+// First three PEB (Process Environment Block) fields
+struct ProcessEnvironmentBlockExcerpt {
+    char flags[8];
+    void* mutant;
+    void* imageBaseAddress;
+};
+
+void* getImageBase() noexcept {
+    // Access the PEB (Process Environment Block) inside the TEB (Thread Environment Block)
+    auto pPEB = reinterpret_cast<ProcessEnvironmentBlockExcerpt*>(__readgsqword(0x60));
+	return pPEB->imageBaseAddress;
+}
